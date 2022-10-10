453cd40b7d88655e6dc9daa42c03d84168ae09cf
Richard @ DMG <79037736+DMG-Richard@users.noreply.github.com>
2022-10-09 23:42:24 -0500

    modified:   hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,25 @@
 print("Hello Richard")
+
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+print('The sum is %.1f' %(float(input('Enter first number: ')) + float(input('Enter second number: '))))
+
